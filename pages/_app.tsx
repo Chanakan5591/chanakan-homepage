9cbf951aad3db90b5e502681a8c377f50a0169d1
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-08-02 11:15:58 +0700

    Just Trying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,15 +8,13 @@ import useAckee from 'use-ackee'
 function App({ Component, pageProps }: AppProps) {
 	const router = useRouter()
 
-	useEffect(() => {
-		useAckee(router.asPath, {
-			server: 'https://analytics.chanakancloud.net',
-			domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
-		}, {
-			detailed: true
-		})
+	useAckee(router.asPath, {
+		server: 'https://analytics.chanakancloud.net',
+		domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
+	}, {
+		detailed: true
+	})
 
-	}, [router.asPath])
 
 	return (
 		<>
