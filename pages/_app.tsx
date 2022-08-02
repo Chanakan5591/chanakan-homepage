c4b26e99861c2d9ecbf3698f9a72aa3fc64a9030
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-08-02 11:01:38 +0700

    Use Programmatic Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import useAckee from 'use-ackee'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
+	const router = useRouter()
 
-      <script async src="https://analytics.chanakancloud.net/tracker.js" data-ackee-server="https://analytics.chanakancloud.net" data-ackee-domain-id="f6707833-45c2-477d-a4db-cd1f39d21733"></script>
-    </>
-  )
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			useAckee(url, {
+				server: 'https://analytics.chanakancloud.net',
+				domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
+			}, {
+				detailed: true
+			})
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
+	return (
+		<>
+			<Navbar />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
