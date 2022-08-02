fc88854752f19f77dc607975c0e4a6319548332a
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-08-02 11:13:28 +0700

    Simplify code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,18 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import useAckee from 'use-ackee'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
 	const router = useRouter()
 
 	useEffect(() => {
-		const handleRouteChange = (url: string) => {
-			useAckee(url, {
-				server: 'https://analytics.chanakancloud.net',
-				domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
-			}, {
-				detailed: true
-			})
-		}
-		router.events.on('routeChangeComplete', handleRouteChange)
-		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange)
-		}
-	}, [router.events])
+		useAckee(router.asPath, {
+			server: 'https://analytics.chanakancloud.net',
+			domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
+		}, {
+			detailed: true
+		})
+
+	}, [router.asPath])
 
 	return (
 		<>
@@ -31,4 +26,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 	)
 }
 
-export default MyApp
+export default App
