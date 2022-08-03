df044c52a6e96805620c217f6ea6c34f3d227cc3
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-08-03 07:11:24 +0700

    Removed Ackee Completely

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import useAckee from 'use-ackee'
 
 function App({ Component, pageProps }: AppProps) {
-	const router = useRouter()
-
-	useAckee(router.asPath, {
-		server: 'https://analytics.chanakancloud.net',
-		domainId: 'f6707833-45c2-477d-a4db-cd1f39d21733',
-	}, {
-		detailed: true
-	})
-
 
 	return (
 		<>
