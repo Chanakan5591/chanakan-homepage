187dc10da65c6e43b8d5d55b909083c056ec40b1
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-08-03 20:02:34 +0700

    Change Analytics URL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<>
-			<script async defer data-website-id="c9223e45-ab29-403c-8e8e-4044633a31a8" src="https://analytics.chanakancloud.net/umami.js"></script>
+			<script async defer data-website-id="c9223e45-ab29-403c-8e8e-4044633a31a8" src="https://api.chanakancloud.net/umami.js"></script>
 			<Navbar />
 			<Component {...pageProps} />
 		</>
