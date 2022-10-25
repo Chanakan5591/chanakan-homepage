cbc358c9e1a5e0bba9b9ed4f20f175fd02094c5d
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-10-25 15:32:52 +0700

    Improved Page Performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Navbar from '../components/navbar'
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<script async defer data-website-id="70adc570-c6be-4c6c-a610-fa652cdbf201" src="https://analytics.chanakancloud.net/science.js"></script>
+			<script async defer data-website-id="04ad9739-87ae-457b-8c32-3407881cfcd1" src="https://analytics.chanakancloud.net/brew.js"></script>
 			<Navbar />
 			<Component {...pageProps} />
 		</>
