7cf019eb54304a9b64978816e0bd429c1efe1781
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-06-08 10:28:03 +0700

    feat(content): add shit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import pfp from '$lib/assets/pfp.jpeg';
 	import { ArrowDown } from 'lucide-svelte';
 </script>
@@ -32,7 +33,9 @@
 </div>
 
 <!-- new -->
-<div class="flex flex-col md:flex-row justify-center md:justify-between max-w-xl mx-auto">
+<div
+	class="flex flex-col gap-x-8 md:flex-row justify-center md:justify-between w-screen max-w-[60%] mx-auto"
+>
 	<div class="flex flex-col">
 		<h1 class="text-5xl title mb-2">Skills</h1>
 		<ul class="text-xl list-disc list-inside">
@@ -43,9 +46,42 @@
 		</ul>
 	</div>
 	<div class="flex flex-col self-end md:self-start mt-4 md:mt-0">
-		<h1 class="text-5xl title">Education</h1>
+		<h1 class="text-5xl title mb-2">Education</h1>
 		<ul class="text-xl list-disc list-inside">
-			<li>yo</li>
+			<li><span class="font-bold">2008 - 2017</span> Prempracha Wattana School</li>
+			<li>
+				<span class="font-bold">2018 - 2020</span> Kanjanapisek Witthayalai Nakornphathom School
+			</li>
+			<li><span class="font-bold">2021 - Present</span> Chitralada Vocational School</li>
 		</ul>
 	</div>
+</div>
+
+<div class="flex flex-col gap-x-8 mt-16 justify-center max-w-xl mx-auto mb-8 pb-8">
+	<span class="text-5xl font-bold font-serif mb-4">Contact Us</span>
+	<form
+		class="flex flex-col gap-y-2 justify-center"
+		action="https://formspree.io/xoqrkkbg"
+		method="post"
+	>
+		<label for="email" class="text-xl font-semibold">Your Email: </label>
+		<input
+			class="p-2 border-black border-2"
+			placeholder="Email here"
+			id="email"
+			type="email"
+			name="email"
+		/>
+		<label for="message" class="text-xl font-semibold">Your message:</label>
+		<textarea
+			id="message"
+			name="message"
+			class="border-black border-2 p-2"
+			rows="10"
+			placeholder="Your message here"
+		/>
+		<button class="p-4 bg-white border-black border-2 w-32 mt-2 font-bold text-lg" type="submit"
+			>Send</button
+		>
+	</form>
 </div>
